e7fde276887b8ec81553780c795f4ac46353f69d
Christopher David Guarini <cdg6285@cs.rit.edu>
2017-04-27 15:43:15 -0400

    Attempting to fix bug where the size is incorrectly calculated, but only for sample.obj. Unable to find why this is.

--- alm.c
+++ alm.c
@@ -17,7 +17,8 @@
 #define read16 fread(&ptr16,2,1,file)
 ///read32 calls fread to read the file for a 32 bit buffer
 #define read32 fread(&ptr32,4,1,file)
-
+///read8 calls fread to read the file for an 8 bit buffer
+#define read8  fread(&ptr8,1,1,file)
 
 char * strcpy(char * dest, const char * src);
 void * memcpy(void *str1, const void *str2, size_t n);
@@ -67,17 +68,18 @@ uint16_t readHW(uint16_t hw){
 ///file
 ///@param : file - pointer to the file
 ///         filename - name of the file
-void readOBJ(FILE * file, char * filename){
+int readOBJ(FILE * file, char * filename){
   //Check if file exists  
   if(!file){//File not found
     fprintf(stderr, "FILE NOT FOUND : %s", filename);
-    return;
+    return 0;
   }
   //Read the file
   exec_t table; //Create the table struct
   //buffers used by fread
   uint16_t ptr16; //16 bit buffer
   uint32_t ptr32; //32 bit buffer
+  uint8_t  ptr8;  //8 bit buffer
 
   //Get magic number
   read16;
@@ -93,7 +95,7 @@ void readOBJ(FILE * file, char * filename){
   if(table.magic != 0xface){
     fprintf(stderr, "error: %s is not an R2K object module (magic number %#6x)\n",
         filename, table.magic);
-    return;
+    return 2;
   }
   
   //print 20 hyphons
@@ -178,27 +180,13 @@ void readOBJ(FILE * file, char * filename){
     uintTable[i] = ptr32;
   }
 
+ 
+
   //Create an array of strings
   char strings[table.sz_strings];
   //Copy uints into string array
   memcpy(strings,uintTable,table.sz_strings);
   
-  //Index the string array
-  int index[table.sz_strings];
-  index[0] = 0;
-  int count = 0;
-  int curr = 1;
-  for(size_t i=0; i<table.sz_strings; i++){
-    //Index every character after nulls
-    if(!strings[i]){//search for null characters
-      index[curr] = count+1;
-      curr++;//increment index cursor
-    }
-    count++;
-  }
-  for(int i=0;i<4;i++){
-    printf("%s\n",strings+index[i]);
-  }
 
 
 
@@ -207,8 +195,100 @@ void readOBJ(FILE * file, char * filename){
   if(table.data[EH_IX_REL]){
     //introductory line
     printf("Relocation information:\n");
+    
+    //Get to relocation section
+    //size is size of file up to the string table
+    size_t relSize = (size - 12*(table.n_syms + table.n_refs + table.n_reloc));
+    fseek(file,relSize,SEEK_SET);//set file pointer to relocation entries
+    //printf("%d\n",relSize);
+    //loop through every entry
+    for(size_t i = 0; i < table.n_reloc; i++){
+      relent_t entry;
+      read32;
+      entry.addr = readWord(ptr32);
+      read8;
+      entry.section = ptr8;
+      read8;
+      entry.type = ptr8;
+      read16;
+      
+      /*
+      //Check type to print
+      char type[10];
+      if(entry.type == REL_IMM){
+        strcpy(type, "IMM");
+      }
+      else if(entry.type == REL_IMM_2){
+        strcpy(type, "IMM_2");
+      }
+      else if(entry.type == REL_WORD){
+        strcpy(type, "WORD");
+      }
+      else if(entry.type == REL_JUMP){
+        strcpy(type, "JUMP");
+      }
+      else{//not defined
+        strcpy(type, "UNKNOWN");
+      }
+      */
+      printf("\t%#010x (%d) type %#06x\n", entry.addr, entry.section, entry.type);
+    }
   }
-  
+
+  //Reference
+  //check if reference table is empty
+  if(table.n_refs){
+    //introductory line
+    printf("Reference information:\n");
+
+    //Get to reference section
+    size_t refSize = (size -12*(table.n_syms + table.n_refs));
+    fseek(file, refSize, SEEK_SET);//set file to reference section
+    //printf("%d\n",refSize); 
+
+    //loop through the entries
+    for( size_t i = 0; i < table.n_refs; i++){
+      refent_t refTable;
+      read32;
+      refTable.addr = readWord(ptr32);
+      read32;
+      refTable.sym = readWord(ptr32);
+      read8;
+      refTable.section = ptr8;
+      read8;
+      refTable.type = ptr8;
+      read16;
+      printf("%#x",refTable.sym);
+      printf("\t%#010x type %#06x symbol %s\n", refTable.addr, refTable.type, strings+refTable.sym);
+    }
+  }
+
+  //Symbol Table
+  //Check if table is empty
+  if(table.n_syms){
+    //Introductory line
+    printf("Symbol table:\n");
+    
+    //get to symbol table section
+    size_t symSize = (size - (table.n_syms * 12));
+    fseek(file, symSize, SEEK_SET);//set file to symbol section
+
+    //Loop through the entries
+    for( size_t i = 0; i < table.n_syms; i++){
+      syment_t sym;
+      read32;
+      sym.flags = readWord(ptr32);
+      read32;
+      sym.value = readWord(ptr32);
+      read32;
+      sym.sym = readWord(ptr32);
+      printf("\tvalue %#010x flags %#010x symbol %s\n", sym.value, sym.flags, strings + sym.sym);
+    }
+  }
+
+  //print final hyphons
+  printf("--------------------\n");
+  return 1;
 
 }
 
@@ -230,7 +310,10 @@ int main(int argc, char * argv[]){
     //open the file
     FILE * file = fopen(argv[i], "r");
     //send to readOBJ to do the heavy lifting
-    readOBJ(file,argv[i]);
+    if(readOBJ(file,argv[i])){
+      fclose(file);
+    }
+    
   }
  
 
